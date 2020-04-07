2a2c8d69843db7dfeace408fa5167c48330c253d
URpineapple <zhang.te@husky.neu.edu>
2020-04-07 16:43:45 -0400

    Updates

---
+++ precache-manifest.94a119bf3350babcd535b8ceb02a56a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae519a39be63057e0d06756c303448fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ade0aa361af3642db2b",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "44df3d6f97491d943537",
+    "url": "/static/css/main.0f64f155.chunk.css"
+  },
+  {
+    "revision": "5ade0aa361af3642db2b",
+    "url": "/static/js/2.98968e41.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.98968e41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44df3d6f97491d943537",
+    "url": "/static/js/main.0549be4f.chunk.js"
+  },
+  {
+    "revision": "ba8e26f4d5633eae8aee",
+    "url": "/static/js/runtime-main.f41275f1.js"
+  }
+]);
